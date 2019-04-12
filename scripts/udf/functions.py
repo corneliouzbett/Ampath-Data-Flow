c1af94d21eaf99523da1eac513679ee016e0c2ae
corneliouzbett <kbett68@gmail.com>
2019-04-12 17:20:33 +0300

    Refactored udf function

--- scripts/udf/functions.py
+++ scripts/udf/functions.py
@@ -3,8 +3,9 @@ import re
 import sys
 
 def searchObsValue(obs, concept):
-    try:
-        found = re.search('## !!'+concept+'=(.+?)!! ##',obs)
-    except AttributeError:
-        found = 'null'
-    return found
+    return re.findall('## !!'+ str(concept) + '=(.+?)!! ##',obs)
+
+obs= '!!1834=7850!! ## !!1915=Patient is awaiting imaging results, then start darcabazine 250 ml every 21 days.!! ## !!1915=Patient is HIV positive since 2016,no presents with pains on the plantar surface and pelvic pain.!! ## !!5096=2013-07-29!! ## !!5219=1068!! ## !!6504=7189!! ## !!6509=6511!! ## !!6575=1107!! ## !!6605=1065!! ## !!7191=MELANOMA OF THE RIGHT HEEL.!! ## !!8723=5622!!'
+bett = searchObsValue(obs, '1915')
+
+print(bett)
