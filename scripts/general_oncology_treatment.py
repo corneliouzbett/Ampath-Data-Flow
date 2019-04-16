4d4bad4423bb657f98d0d9acb811475c04b62eb7
corneliouzbett <kbett68@gmail.com>
2019-04-16 11:44:44 +0300

    Refactored gen oncology

---
+++ scripts/general_oncology_treatment.py
@@ -0,0 +1,27 @@
+from shared.spark_commons import commons
+from shared.amrs_obs import AmrsObs
+
+class GeneralOncologyTreatment(AmrsObs):
+    
+    def __init__(self, sql_context):
+        AmrsObs.__init__(self, sql_context)
+
+
+    def generate_general_oncology_treatment_df(self):
+        print('===================== creating general oncology treatment dataframe from amrs obs =================================')
+        super(GeneralOncologyTreatment, self).generate_obs_dataframe()\
+            .createGlobalTempView("general_oncology_treatment_temp")
+        gen_onc_tr_df = self.sql_context.sql(
+            """
+            SELECT * FROM global_temp.general_oncology_treatment_temp where encounter_id in (42, 43)
+            """
+        )
+        gen_onc_tr_df.printSchema()
+        return gen_onc_tr_df
+
+
+common = commons('general_oncology_treatment')
+context = common.create_context()
+
+general_oncology_treatment = LungCancerTreatment(context)
+general_oncology_treatment.generate_lung_cancer_treatment_df()
