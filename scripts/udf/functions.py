89e767caea436a88269c1b287b1d4e4fd3b71f98
corneliouzbett <kbett68@gmail.com>
2019-04-16 09:51:41 +0300

    added amrs obs scripts

--- scripts/udf/functions.py
+++ scripts/udf/functions.py
@@ -2,10 +2,10 @@ import os
 import re
 import sys
 
-def searchObsValue(obs, concept):
+def search_obs_value(obs, concept):
     return re.findall('## !!'+ str(concept) + '=(.+?)!! ##',obs)
 
 obs= '!!1834=7850!! ## !!1915=Patient is awaiting imaging results, then start darcabazine 250 ml every 21 days.!! ## !!1915=Patient is HIV positive since 2016,no presents with pains on the plantar surface and pelvic pain.!! ## !!5096=2013-07-29!! ## !!5219=1068!! ## !!6504=7189!! ## !!6509=6511!! ## !!6575=1107!! ## !!6605=1065!! ## !!7191=MELANOMA OF THE RIGHT HEEL.!! ## !!8723=5622!!'
-bett = searchObsValue(obs, '1915')
+bett = search_obs_value(obs, '1915')
 
 print(bett)
